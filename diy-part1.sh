552f3b545d5043d9e968087572b2150ccc04bd40
leopardciaw <47556424+leopardciaw@users.noreply.github.com>
2021-10-05 22:07:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,7 +25,7 @@ git clone https://github.com/kenzok8/small.git package/small
 # git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
 
 #  增加luci-app-unblockneteasemusic，来源immortalwrt
-git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+# git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
 # CONFIG_PACKAGE_luci-app-filebrowser=y
 git clone https://github.com/immortalwrt/openwrt-filebrowser.git package/luci-app-filebrowser
